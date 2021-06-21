2def125c6091817061ac71142a7a9cf0fca1fc91
Rafi Rabbani <rafirabbani103@gmail.com>
2021-06-22 04:28:02 +0700

    add logout on admin sidebar

--- client/views/Admin/Components/AdminSidebar.js
+++ client/views/Admin/Components/AdminSidebar.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import AuthActions from '../../../Actions/AuthActions'
 import Popcorn from '../../../assets/images/popcorn-png-3.png'
-import { HomeIcon, UsersIcon, FilmIcon, SparklesIcon, AnnotationIcon, LogoutIcon } from '@heroicons/react/solid'
+import { FilmIcon, SparklesIcon, LogoutIcon } from '@heroicons/react/solid'
 import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
+    const history = useHistory()
+    const dispatch = useDispatch()
+    
+    const logOut = () => {
+        dispatch(AuthActions.signOut()).then(() => {
+            history.push('/mini-project/signin')
+        })
+    }
+    
     return (
         <div>
             <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:w-48 py-18 px-6 bg-black">
@@ -36,19 +48,9 @@ export default function Sidebar() {
                                         "text-l flex items-center not-italic text-red-600"
                                     }><SparklesIcon className={'h-5 w-5 text-gray-500 mr-2'}/>Casts</i>{""}                        
                         </Link>
-                        <Link
-                                className={
-                                    "text-l py-3 block font-bold hover:bg-gray-200 rounded-xl"
-                                }
-                                to="/mini-project/admin/users/"
-                            >
-                                <i className={
-                                        "text-l flex items-center not-italic text-red-600"
-                                    }><UsersIcon className={'h-5 w-5 text-gray-500 mr-2'}/>Users</i>{""}                        
-                        </Link>
                         </li>
                         <button className='flex items-center text-red-600 text-l py-3 font-bold hover:bg-gray-200 rounded-xl'
-                        /* onClick={logOut} */>
+                        onClick={logOut}>
                             <LogoutIcon className='h-5 w-5 text-gray-500 mr-2'/>
                             <span>Log Out</span>
                         </button>
