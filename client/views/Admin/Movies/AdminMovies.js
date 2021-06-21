4745d0de1694b926d643fe32c93f431e6e7d9f21
Rafi Rabbani <rafirabbani103@gmail.com>
2021-06-22 04:21:34 +0700

    admin movies page pagination done

--- client/views/Admin/Movies/AdminMovies.js
+++ client/views/Admin/Movies/AdminMovies.js
@@ -8,6 +8,9 @@ import AdminUpdateMovie from './AdminUpdateMovie'
 import MainLayout from '../Components/AdminMainLayout'
 
 export default function Movies() {
+    const [movies, setMovies] = useState([])
+    const [pages, setPages] = useState([])
+    const [pageChange, setPageChange] = useState(false)
     const [createMovieModal, setCreateMovieModal] = useState(false)
     const [updateMovieModal, setUpdateMovieModal] = useState(false)
     const [update, setUpdate] = useState(false)
@@ -30,21 +33,32 @@ export default function Movies() {
     })
     const { movie } = useSelector((state) => state)
     const dispatch = useDispatch()
-    const { movies, createMovie, updateMovie, deleteMovie } = movie
+    const { movieData, createMovie, updateMovie, deleteMovie } = movie
+    
 
     useEffect(() => {
-        dispatch(MovieActions.movieList());
+        dispatch(MovieActions.movieList(0));
     }, [])
 
     useEffect(() => { 
-        if (!movies) {
-            dispatch(MovieActions.movieList())
+        if (!movieData) {
+            dispatch(MovieActions.movieList(0))
         }
+        setPageChange(false)
         setUpdate(false)
     }, [update, createMovie, updateMovie, deleteMovie])
 
-    const handleDetail = (movieID, movieTitle, movieEpisode, movieDirector, movieStudio, movieTVStatus, movieDuration, 
-        movieRelease, movieCountry,movieGenre, movieRating, movieNetwork, movieTrailer, 
+    useEffect(() => {   
+        if (movieData) {
+            const { movies, totalPages } = movieData
+            setMovies(movies)
+            setPages([...Array(totalPages).keys()])
+            setPageChange(false)
+        }
+    }, [movieData])
+
+    const handleDetail = (movieID, movieTitle, movieEpisode, movieDirector, movieStudio, movieTVStatus, movieDuration,
+        movieRelease, movieCountry,movieGenre, movieRating, movieNetwork, movieTrailer,
         movieViews, moviePrice) => {
             setUpdateMovieModal(true)
             setDetailMovie({
@@ -63,20 +77,25 @@ export default function Movies() {
                 movie_trailer: movieTrailer,
                 movie_views: movieViews,
                 movie_price: moviePrice,
-            })
-            
-        }
+            })   
+    }
 
-        const onDestroy = (id) => {
-            dispatch(MovieActions.deleteMovie(id)).then((result) => {
-                if (result.status === 200) {
-                    alert(`Movie Delete`)
-                }
-                else {
-                    alert(`Delete Fail`)
-                }
-            })
-        }
+    const onDestroy = (id) => {
+        dispatch(MovieActions.deleteMovie(id)).then((result) => {
+            if (result.status === 200) {
+                alert(`Movie Delete`)
+            }
+            else {
+                alert(`Delete Fail`)
+            }
+        })
+    }
+
+    const onPageChange = (page) => {
+        dispatch(MovieActions.movieList(page))
+        setPageChange(true)
+    }
+
 
     return (
         <div>
@@ -161,6 +180,18 @@ export default function Movies() {
                                         ))}
                                     </tbody>
                                 </table>
+                            </div>
+                            <div className="flex flex-row items-center justify-center mt-5">
+                                <label>Pages:</label>
+                                <div className="flex flex-row items-center justify-between">
+                                    {
+                                        pages && pages.map((page) => (
+                                            <div className="mx-3" key={page}>
+                                                <button className="focus:outline-none hover:underline focus:underline" onClick={()=> onPageChange(page)}>{page+1}</button>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
                             </div>
                         </div>
                     </div>
