fadc377887d4bda68ab59f35aec2eae1ac56452a
Rafi Rabbani <rafirabbani103@gmail.com>
2021-07-02 16:42:26 +0700

    change db pass on config-db

--- config/config-db.js
+++ config/config-db.js
@@ -5,7 +5,7 @@ import config from './config'
 const sequelize = new Sequelize({
   database: "d5hl1q5624h5se",
   username: "ctlefhhqsiumnk",
-  password: "7644bd5514993de4227f29b4c098f5a25523c85b648d4e59994918899a05f883",
+  password: "8257e77721cace524b3879cc4f0f2601cb7eb0332f112c76b6707b11e19810ea",
   host: "ec2-50-17-255-120.compute-1.amazonaws.com",
   port: 5432,
   dialect: "postgres",
