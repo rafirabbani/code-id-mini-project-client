658a6d286f2c4ae81371771b9265104d40d5962b
Rafi Rabbani <rafirabbani103@gmail.com>
2021-07-02 18:03:14 +0700

    add render method for prod

--- client/main.js
+++ client/main.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { hydrate } from 'react-dom'
+import { hydrate, render } from 'react-dom'
 import App from './App'
 import './assets/styles/index.css';
 import { Provider } from 'react-redux'
 import store from './ReduxStore'
+
+const renderMethod = !!module.hot ? render : hydrate;
 
 // Create Redux Store
 //const store = createStore(reducer, /* +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() */)
