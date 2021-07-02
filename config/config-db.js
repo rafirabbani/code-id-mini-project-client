f6ad2dddf070f672d0c8c02818d8d90df45f9e4b
Rafi Rabbani <rafirabbani103@gmail.com>
2021-07-02 16:37:57 +0700

    add heroku psotgre config-db

--- config/config-db.js
+++ config/config-db.js
@@ -2,14 +2,20 @@ import Sequelize from 'sequelize';
 import config from './config'
 
 // config database
-const sequelize = new Sequelize(
-    config.db_name,
-    config.db_username,
-    config.db_password,
-    {
-      dialect: 'postgres',
+const sequelize = new Sequelize({
+  database: "d5hl1q5624h5se",
+  username: "ctlefhhqsiumnk",
+  password: "7644bd5514993de4227f29b4c098f5a25523c85b648d4e59994918899a05f883",
+  host: "ec2-50-17-255-120.compute-1.amazonaws.com",
+  port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
     },
-  );
+  },
+});
   
   sequelize
     .authenticate()
