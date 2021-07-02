a51f6a70ba476bff009b78351bb334513d211bf6
Rafi Rabbani <rafirabbani103@gmail.com>
2021-07-02 16:31:21 +0700

    change config db to heroku postgres

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 5000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    db_name : "mini-project",
-    db_username : "postgres",
-    db_password: "1234"
+    db_name : "d5hl1q5624h5se",
+    db_username : "ctlefhhqsiumnk",
+    db_password: "8257e77721cace524b3879cc4f0f2601cb7eb0332f112c76b6707b11e19810ea"
   }
   
   export default config
